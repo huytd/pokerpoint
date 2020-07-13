a352ea48a1d6cd0edeaed701c4b4308bab928cef
Huy Tran <kingbazoka@gmail.com>
2020-07-13 10:43:16 -0400

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use actix::prelude::*;
+use actix_files::Files;
+use actix_web_actors::ws;
+use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
+
+async fn client_route(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    let addr = "0.0.0.0:1234";
+    HttpServer::new(move || {
+        App::new()
+            .service(web::resource("/ws").to(client_route))
+            .service(Files::new("/", "./static/").index_file("index.html"))
+    })
+    .bind(&addr)?
+    .run()
+    .await
+}
